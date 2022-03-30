c9de239797b11d9633b2481cbf4cec7a1fca9526
vokarpenko <dcoqge9x>
2022-03-30 14:02:08 +0300

    delete

--- lib/dart_file.dart
+++ lib/dart_file.dart
@@ -1,13 +1,3 @@
 class Feature {
-  int hello(String str) {
-    return 1 + 1;
-  }
-
-  void main() {}
-
-  void log() {
-    throw UnimplementedError();
-  }
-
-  void func1() {}
+  void func1;
 }
